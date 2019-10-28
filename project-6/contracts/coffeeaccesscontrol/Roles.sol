abd0c79fee590096f1fbe79d2771020f0cfd349f
Cloud <zhangylcloud@outlook.com>
2019-10-27 18:07:58 -0700

    in progress with SupplyChain.sol

--- project-6/contracts/coffeeaccesscontrol/Roles.sol
+++ project-6/contracts/coffeeaccesscontrol/Roles.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.4.24;
-
+pragma solidity ^0.5.8;
 /**
  * @title Roles
  * @dev Library for managing addresses assigned to a Role.
--- project-6/contracts/coffeecore/Ownable.sol
+++ project-6/contracts/coffeecore/Ownable.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.4.24;
-
+pragma solidity ^0.5.8;
 /// Provides basic authorization control
 contract Ownable {
     address private origOwner;
